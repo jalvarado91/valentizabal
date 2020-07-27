22905b86e9e7f94b77d99847a02af228c0082133
Juan Alvarado <jalvarado91@gmail.com>
2020-07-26 20:00:27 -0400

    Title

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
 import "../styles/index.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="antialiased bg-cherry" >
+    <div className="antialiased bg-cherry">
+      <Head>
+        <title>Valentizabal</title>
+        <meta name="theme-color" content="#E12D58" />
+      </Head>
       <Component {...pageProps} />
     </div>
   );
